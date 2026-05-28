34b4234cf11c28a07dcb31d859c34c1cfca704fa
wzhangyubin@163.com <wzhangyubin@163.com>
2026-05-28 17:15:20 +0800

    更改game14 分类页样式

--- game3/public/js/BaseURL.js
+++ game3/public/js/BaseURL.js
@@ -1,17 +1,28 @@
+
+export const BASE_URL = "https://datajson.pages.dev/games10-data.json";
+
+const baseUrl = 'https://games10-1rg.pages.dev';
+
+
+
+// export const BASE_URL = "https://datajson.pages.dev/games11/games11-data.json";
+// const baseUrl = 'https://games11-dzd.pages.dev';
+
+// export const BASE_URL = "https://datajson.pages.dev/games8/db9.json";
+// const baseUrl = 'https://games8-9t4.pages.dev';
+
+
+
 // export const BASE_URL = "https://datajson.pages.dev/games7/db9.json";
 // const baseUrl = 'https://games7.pages.dev';
 
-export const BASE_URL = "https://datajson.pages.dev/games8/db9.json";
-const baseUrl = 'https://games8-9t4.pages.dev';
+
 
 
 // export const BASE_URL = "https://datajson.pages.dev/games9-data.json";
 
 // const baseUrl = 'https://games9-4av.pages.dev';
 
-// export const BASE_URL = "https://datajson.pages.dev/games10-data.json";
-
-// const baseUrl = 'https://games10-1rg.pages.dev';
 
 
 
