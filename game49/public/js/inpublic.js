7dedaaf6246ee4cc5b2fbe9a4e9fc813898f6483
wzhangyubin@163.com <wzhangyubin@163.com>
2026-06-11 15:42:31 +0800

    fix 49

--- game49/public/js/inpublic.js
+++ game49/public/js/inpublic.js
@@ -222,24 +222,14 @@ export async function initCategoryPage() {
 
         const detailPath = getDetailPagePath();
         const gamesHTML = games.map((game, index) => {
+            const description = game.description ? game.description.substring(0, 50) + '...' : '';
             let html = `
             <a href="${detailPath}?id=${game.id}${window.channel ? '&channel=' + window.channel : ''}" class="game-card">
-                <img src="${game.image}" alt="${game.name}">
                 <div class="game-card-content">
                     <p class="game-title">${game.name}</p>
-                    <div class="game-stats">
-                        <svg width="20" height="20" viewBox="0 0 1024 1024" fill="#ff5252" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M 832 364.8 h -147.2 s 19.2 -64 32 -179.2 c 6.4 -57.6 -38.4 -115.2 -102.4 -121.6 h -12.8 c -51.2 0 -83.2 32 -102.4 76.8 l -38.4 96 c -32 64 -57.6 102.4 -76.8 115.2 c -25.6 12.8 -121.6 12.8 -128 12.8 H 128 c -38.4 0 -64 25.6 -64 57.6 v 480 c 0 32 25.6 57.6 64 57.6 h 646.4 c 96 0 121.6 -64 134.4 -153.6 l 51.2 -307.2 c 6.4 -70.4 -6.4 -134.4 -128 -134.4 Z m -576 537.6 H 128 V 422.4 h 128 v 480 Z m 640 -409.6 l -51.2 307.2 c -12.8 57.6 -12.8 102.4 -76.8 102.4 H 320 V 422.4 c 44.8 0 70.4 -6.4 89.6 -19.2 c 32 -12.8 64 -64 108.8 -147.2 c 25.6 -64 38.4 -96 44.8 -102.4 c 6.4 -19.2 19.2 -32 44.8 -32 h 6.4 c 32 0 44.8 32 44.8 51.2 c -12.8 102.4 -32 166.4 -32 166.4 l -25.6 83.2 h 243.2 c 19.2 0 32 0 44.8 12.8 c 12.8 12.8 6.4 38.4 6.4 57.6 Z" stroke="#ff5252" stroke-width="60" stroke-linejoin="round" stroke-linecap="round"/>
-                        </svg>
-                        <span class="game-download">${game.downloads || '0'}</span>
-                        <span class="game-rating">
-                            <svg width="24" height="24" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M544.402286 99.693714a73.142857 73.142857 0 0 1 33.206857 33.206857l84.845714 171.958858 189.805714 27.648a73.142857 73.142857 0 0 1 40.521143 124.708571l-137.289143 133.851429 32.402286 189.074285a73.142857 73.142857 0 0 1-106.130286 77.092572L512 768l-169.764571 89.234286a73.142857 73.142857 0 0 1-106.130286-77.092572l32.402286-189.001143-137.289143-133.851428a73.142857 73.142857 0 0 1 40.521143-124.781714l189.805714-27.648 84.845714-171.958858a73.142857 73.142857 0 0 1 98.011429-33.206857z m69.485714 272.091429L512 165.302857 410.112 371.712l-227.84 33.133714 164.864 160.694857-38.912 226.962286L512 685.348571l203.776 107.154286-38.912-226.962286 164.864-160.694857-227.84-33.133714z" fill="#ffab2a" stroke="#ffab2a" stroke-width="40"/>
-                            </svg>
-                            ${game.rating || '5.0'}
-                        </span>
-                    </div>
+                    <div class="homepage-game-description">${description}</div>
                 </div>
+                <img src="${game.image}" alt="${game.name}">
             </a>
         `;
             if (index === 1) {
