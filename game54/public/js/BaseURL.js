14d8f09991a56ab66f9296d5b9985d20a21fb1ba
1565721236-cmyk <1565721236@qq.com>
2026-06-24 16:55:04 +0800

    update

--- game54/public/js/BaseURL.js
+++ game54/public/js/BaseURL.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://datajson.pages.dev/games9/db13.json";
+export const BASE_URL = "https://datajson.pages.dev/games9/db1.json";
 
 const baseUrl = 'https://games9-4av.pages.dev';
 
@@ -63,7 +63,7 @@ const DEFAULT_CATEGORIES = [
 
 
 export async function getCategoryOrder() {
-  const infoType = 'info5';
+  const infoType = 'info3';
   try {
      const data = await loadGameData();
     if (Array.isArray(data) && data.length > 0 && data[0] && data[0][infoType]) {
